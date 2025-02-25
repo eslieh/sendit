bd9beadd9367ff6298b64fa8870a1bafe055307c
mashfied <victoreslieh@gmail.com>
2025-02-25 23:37:03 +0300

    SEN-12 get a quote

--- client/src/pages/user/index.js
+++ client/src/pages/user/index.js
@@ -59,7 +59,7 @@ const UserHome = () => {
         pickupLocation,
         deliveryLocation,
         packageDescription,
-        status: "In Transit",
+        status: "requested",
         distance: routeDistance,
       };
       const endpoint = "/orders/quote";
