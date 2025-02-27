5eecaee482719e5aa74caacfdfcce38a98914b64
maing1 <faithmaingi1622@gmail.com>
2025-02-27 21:40:41 +0300

    more changes

--- server/app.py
+++ server/app.py
@@ -5,17 +5,29 @@ from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from models import db
 from flask_cors import CORS
+from datetime import timedelta
 
 
 from routes.auth_routes import init_auth_routes
 from routes.wallet_routes import init_wallet_routes
 from routes.order_routes import init_order_routes
+from routes.courier_routes import init_courier_routes
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SECRET_KEY'] = 'your_strong_secret_key'
 app.config["JWT_SECRET_KEY"] = 'your_jwt_secret_key'
 app.config['JWT_TOKEN_LOCATION'] = ['headers']
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=2)
+
+# # Initialize Flask-Mail
+# app.config['MAIL_SERVER'] = 'localhost'
+# app.config['MAIL_PORT'] = 1025  # Change to your local SMTP port
+# app.config['MAIL_USE_TLS'] = False
+# app.config['MAIL_USE_SSL'] = False
+# app.config['MAIL_USERNAME'] = None
+# app.config['MAIL_PASSWORD'] = None
+
 
 jwt = JWTManager(app)
 db.init_app(app)
@@ -25,6 +37,8 @@ api = Api(app)
 bcrypt = Bcrypt(app)
 CORS(app)
 
+
+
 @app.route('/')
 def index():
     return '<h1>Send it</h1>'
@@ -33,6 +47,7 @@ def index():
 init_auth_routes(app, bcrypt)
 init_wallet_routes(app)
 init_order_routes(app)
+init_courier_routes(app)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
